6f9e18364f41c2f3dfae979b95503cfc690e35f7
Sami Salonen <ssalonen@gmail.com>
2022-12-28 21:23:11 +0200

    [modbus] WIP: #14058
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- bundles/org.openhab.binding.modbus/src/main/java/org/openhab/binding/modbus/internal/handler/AbstractModbusEndpointThingHandler.java
+++ bundles/org.openhab.binding.modbus/src/main/java/org/openhab/binding/modbus/internal/handler/AbstractModbusEndpointThingHandler.java
@@ -12,19 +12,30 @@
  */
 package org.openhab.binding.modbus.internal.handler;
 
+import java.util.concurrent.Future;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.binding.modbus.handler.EndpointNotInitializedException;
 import org.openhab.binding.modbus.handler.ModbusEndpointThingHandler;
 import org.openhab.binding.modbus.internal.ModbusConfigurationException;
+import org.openhab.core.io.transport.modbus.AsyncModbusFailure;
 import org.openhab.core.io.transport.modbus.ModbusCommunicationInterface;
+import org.openhab.core.io.transport.modbus.ModbusCommunicationInterfaceDelegate;
+import org.openhab.core.io.transport.modbus.ModbusFailureCallback;
 import org.openhab.core.io.transport.modbus.ModbusManager;
+import org.openhab.core.io.transport.modbus.ModbusReadCallback;
+import org.openhab.core.io.transport.modbus.ModbusReadRequestBlueprint;
+import org.openhab.core.io.transport.modbus.ModbusWriteCallback;
+import org.openhab.core.io.transport.modbus.ModbusWriteRequestBlueprint;
+import org.openhab.core.io.transport.modbus.PollTask;
 import org.openhab.core.io.transport.modbus.endpoint.EndpointPoolConfiguration;
 import org.openhab.core.io.transport.modbus.endpoint.ModbusSlaveEndpoint;
 import org.openhab.core.thing.Bridge;
 import org.openhab.core.thing.ChannelUID;
 import org.openhab.core.thing.ThingStatus;
 import org.openhab.core.thing.ThingStatusDetail;
+import org.openhab.core.thing.ThingStatusInfo;
 import org.openhab.core.thing.binding.BaseBridgeHandler;
 import org.openhab.core.types.Command;
 import org.slf4j.Logger;
@@ -41,6 +52,65 @@ import org.slf4j.LoggerFactory;
 @NonNullByDefault
 public abstract class AbstractModbusEndpointThingHandler<E extends ModbusSlaveEndpoint, C> extends BaseBridgeHandler
         implements ModbusEndpointThingHandler {
+
+    /**
+     * Listen for communication events and update thing status accordingly
+     *
+     * @author Sami Salonen - Initial contribution
+     *
+     */
+    private class CommunicationInterfaceWrapper extends ModbusCommunicationInterfaceDelegate {
+
+        public CommunicationInterfaceWrapper(ModbusCommunicationInterface wrapped) {
+            super(wrapped);
+        }
+
+        @Override
+        public PollTask registerRegularPoll(ModbusReadRequestBlueprint request, long pollPeriodMillis,
+                long initialDelayMillis, ModbusReadCallback resultCallback,
+                ModbusFailureCallback<ModbusReadRequestBlueprint> failureCallback) {
+            return super.registerRegularPoll(request, pollPeriodMillis, initialDelayMillis,
+                    wrapReadSuccessCallback(resultCallback), wrapReadFailureCallback(failureCallback));
+        }
+
+        @Override
+        public Future<?> submitOneTimePoll(ModbusReadRequestBlueprint request, ModbusReadCallback resultCallback,
+                ModbusFailureCallback<ModbusReadRequestBlueprint> failureCallback) {
+            return super.submitOneTimePoll(request, wrapReadSuccessCallback(resultCallback),
+                    wrapReadFailureCallback(failureCallback));
+        }
+
+        @Override
+        public Future<?> submitOneTimeWrite(ModbusWriteRequestBlueprint request, ModbusWriteCallback resultCallback,
+                ModbusFailureCallback<ModbusWriteRequestBlueprint> failureCallback) {
+            return super.submitOneTimeWrite(request, wrapWriteSuccessCallback(resultCallback),
+                    wrapReadFailureCallback(failureCallback));
+        }
+
+        private <R> ModbusFailureCallback<R> wrapReadFailureCallback(ModbusFailureCallback<R> failureCallback) {
+            ModbusFailureCallback<R> wrappedFailureCallback = result -> {
+                AbstractModbusEndpointThingHandler.this.handleCommunicationError(result);
+                failureCallback.handle(result);
+            };
+            return wrappedFailureCallback;
+        }
+
+        private ModbusReadCallback wrapReadSuccessCallback(ModbusReadCallback resultCallback) {
+            ModbusReadCallback wrappedResultCallback = result -> {
+                AbstractModbusEndpointThingHandler.this.resetCommunicationErrorOrUnknown();
+                resultCallback.handle(result);
+            };
+            return wrappedResultCallback;
+        }
+
+        private ModbusWriteCallback wrapWriteSuccessCallback(ModbusWriteCallback resultCallback) {
+            ModbusWriteCallback wrappedResultCallback = result -> {
+                AbstractModbusEndpointThingHandler.this.resetCommunicationErrorOrUnknown();
+                resultCallback.handle(result);
+            };
+            return wrappedResultCallback;
+        }
+    }
 
     protected volatile @Nullable C config;
     protected volatile @Nullable E endpoint;
@@ -75,8 +145,9 @@ public abstract class AbstractModbusEndpointThingHandler<E extends ModbusSlaveEn
                     throw new IllegalStateException("endpoint null after configuration!");
                 }
                 try {
-                    comms = modbusManager.newModbusCommunicationInterface(endpoint, poolConfiguration);
-                    updateStatus(ThingStatus.ONLINE);
+                    comms = new CommunicationInterfaceWrapper(
+                            modbusManager.newModbusCommunicationInterface(endpoint, poolConfiguration));
+                    updateStatus(ThingStatus.UNKNOWN);
                 } catch (IllegalArgumentException e) {
                     updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
                             formatConflictingParameterError());
@@ -113,6 +184,21 @@ public abstract class AbstractModbusEndpointThingHandler<E extends ModbusSlaveEn
     @Nullable
     public E getEndpoint() {
         return endpoint;
+    }
+
+    private void resetCommunicationErrorOrUnknown() {
+        ThingStatusInfo statusInfo = thing.getStatusInfo();
+        if (ThingStatus.UNKNOWN.equals(statusInfo.getStatus()) || (ThingStatus.OFFLINE.equals(statusInfo.getStatus())
+                && ThingStatusDetail.COMMUNICATION_ERROR.equals(statusInfo.getStatusDetail()))) {
+            updateStatus(ThingStatus.ONLINE);
+        }
+    }
+
+    private <R> void handleCommunicationError(AsyncModbusFailure<R> result) {
+        ThingStatusInfo statusInfo = thing.getStatusInfo();
+        if (!ThingStatus.OFFLINE.equals(statusInfo.getStatus())) {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, result.toString());
+        }
     }
 
     @Override
