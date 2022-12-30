77048b76af56683401b7098339ff10c0e6300e3b
Sami Salonen <ssalonen@gmail.com>
2022-12-30 08:39:54 +0200

    Implement read parameter validation for channel
    Migrated relevant tests as unit tests.
    
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

---
+++ bundles/org.openhab.binding.modbus/src/test/java/org/openhab/binding/modbus/internal/ReadChannelHandlerReadParametersValidationTest.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.internal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openhab.core.io.transport.modbus.ModbusConstants.ValueType;
+import org.openhab.core.io.transport.modbus.ModbusReadFunctionCode;
+
+/**
+ * @author Sami Salonen - Initial contribution
+ */
+@NonNullByDefault
+public class ReadChannelHandlerReadParametersValidationTest {
+    private static Stream<Arguments> provideArgsForTestValidateConfig()
+
+    {
+        // expected validity, poll function, poll start, poll length, channel start address, channel value type
+        Arguments.of(true, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "6.1", ValueType.BIT);
+        return Stream.of(
+        //@formatter:off
+                //
+                // reading coils 4, 5, 6. Coil 7 & 8 are out of bounds. Coil 6 within bounds
+                //
+                Arguments.of(false, ModbusReadFunctionCode.READ_COILS, 4, 3, "8", ValueType.BIT),
+                Arguments.of(false, ModbusReadFunctionCode.READ_COILS, 4, 3, "7", ValueType.BIT),
+                Arguments.of(true, ModbusReadFunctionCode.READ_COILS, 4, 3, "6", ValueType.BIT),
+                //
+                // reading registers 4, 5, 6, and decoding individual BIT from registers
+                // - Register 8 bit 0: out of bounds
+                // - 7.16 out of bounds: bit 16 over the register (bit index 15 is max)
+                // - 6.16 out of bounds: similarly invalid
+                // - 6.15 OK: most significant bit of register 6
+                // - 6.0 OK: least significant bit of register 6
+                // - 6.1 OK: 2nd least significant bit of register 6
+                //
+                Arguments.of(false, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "8.0", ValueType.BIT),
+                Arguments.of(false, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "7.16", ValueType.BIT),
+                Arguments.of(false, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "6.16", ValueType.BIT),
+                Arguments.of(true, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "6.15", ValueType.BIT),
+                Arguments.of(true, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "6.0", ValueType.BIT),
+                //
+                // reading registers 4, 5, 6, and decoding with INT16
+                // - register 6 OK
+                // - register 7 & 8 out of bounds
+                // - address 6.0 invalid (not expecting sub-index)
+                Arguments.of(true, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "6", ValueType.INT16),
+                Arguments.of(false, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "7", ValueType.INT16),
+                Arguments.of(false, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "8", ValueType.INT16),
+                Arguments.of(false, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "6.0", ValueType.INT16),
+                //
+                // reading registers 4, 5, 6, and decoding with INT32
+                // - address 5 OK (decodes registers 5 & 6 as INT32)
+                // - address 6 out-of-bounds (would need register 7 for decoding INT32)
+                Arguments.of(true, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "5", ValueType.INT32),
+                Arguments.of(false, ModbusReadFunctionCode.READ_MULTIPLE_REGISTERS, 4, 3, "6", ValueType.INT32)
+                //@formatter:on
+        );
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgsForTestValidateConfig")
+    public void testValidateConfig(boolean expectedValidity, ModbusReadFunctionCode pollerFunctionCode, int pollerStart,
+            int pollerLength, String channelStart, ValueType channelValueType) {
+        if (!expectedValidity) {
+            assertThrows(ModbusConfigurationException.class, () -> {
+                ReadChannelHandler.validateConfigCase1(pollerFunctionCode, pollerStart, pollerLength, channelStart,
+                        channelValueType);
+            });
+        } else {
+            final boolean[] actualValidity = new boolean[1];
+            assertDoesNotThrow(() -> {
+                actualValidity[0] = ReadChannelHandler.validateConfigCase1(pollerFunctionCode, pollerStart,
+                        pollerLength, channelStart, channelValueType);
+            });
+            assertEquals(expectedValidity, actualValidity[0]);
+        }
+
+    }
+
+}
