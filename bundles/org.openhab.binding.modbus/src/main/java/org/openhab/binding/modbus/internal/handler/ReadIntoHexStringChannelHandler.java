e9ce04ac69934c49e3449e679df44200d340da74
Sami Salonen <ssalonen@gmail.com>
2023-02-12 09:31:16 +0200

    WIP hex string support
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

---
+++ bundles/org.openhab.binding.modbus/src/main/java/org/openhab/binding/modbus/internal/handler/ReadIntoHexStringChannelHandler.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.internal.handler;
+
+import java.util.function.Consumer;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.binding.modbus.config.ReadChannelConfiguration;
+import org.openhab.core.io.transport.modbus.BitArray;
+import org.openhab.core.io.transport.modbus.ModbusRegisterArray;
+import org.openhab.core.library.types.StringType;
+import org.openhab.core.types.State;
+import org.openhab.core.types.UnDefType;
+import org.openhab.core.util.HexUtils;
+
+/**
+ * Handler for readIntoNumber channels, decoding raw binary data from modbus according to channel configuration.
+ *
+ * @author Sami Salonen - Initial contribution
+ *
+ */
+@NonNullByDefault
+public class ReadIntoHexStringChannelHandler extends ReadIntoChannelHandler {
+
+    private int relativeStartIndex;
+
+    public ReadIntoHexStringChannelHandler(int pollStart, ReadChannelConfiguration config,
+            Consumer<State> stateUpdater) {
+        super(pollStart, config, stateUpdater);
+        relativeStartIndex = parsedAddress.channelStartElement - pollStart;
+    }
+
+    @Override
+    public void process(BitArray bits) {
+        processUpdatedValue(HexUtils.bytesToHex(bits.copyOfRange(0, relativeStartIndex + config.length).getBytes()));
+    }
+
+    @Override
+    public void process(ModbusRegisterArray registers) {
+        processUpdatedValue(
+                registers.copyOfRange(relativeStartIndex, relativeStartIndex + config.length).toHexString());
+    }
+
+    @Override
+    public void process(Exception readError) {
+        processUpdatedValue(UnDefType.UNDEF);
+    }
+
+    private void processUpdatedValue(String hexString) {
+        updateExpiredChannel(System.currentTimeMillis(), new StringType(hexString));
+    }
+
+}
---
+++ bundles/org.openhab.binding.modbus/src/test/java/org/openhab/binding/modbus/internal/handler/ReadIntoHexStringChannelHandlerTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.internal.handler;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.library.types.DecimalType;
+import org.openhab.core.library.types.OnOffType;
+import org.openhab.core.types.UnDefType;;
+
+/**
+ * @author Sami Salonen - Initial contribution
+ */
+@NonNullByDefault
+public class ReadIntoHexStringChannelHandlerTest {
+
+    public static Collection<Object[]> provideArgsForPostProcessTest() {
+        return Collections.unmodifiableList(Stream.of(
+        //@formatter:off
+                                new Object[] { OnOffType.OFF, DecimalType.ZERO, BigDecimal.valueOf(0), false},
+                                new Object[] { OnOffType.ON, DecimalType.ZERO, BigDecimal.valueOf(0), true},
+                                new Object[] { OnOffType.ON, DecimalType.valueOf("3.14"), BigDecimal.valueOf(0), false},
+                                new Object[] { OnOffType.OFF, DecimalType.valueOf("3.14"), BigDecimal.valueOf(0), true},
+                                new Object[] { OnOffType.OFF, DecimalType.valueOf("3.14"), new BigDecimal("3.14"), false},
+                                new Object[] { OnOffType.ON, DecimalType.valueOf("3.14"),  new BigDecimal("3.14"), true},
+                                new Object[] { OnOffType.OFF, DecimalType.valueOf("-3.14"), new BigDecimal("-3.14"), false},
+                                new Object[] { OnOffType.ON, DecimalType.valueOf("-3.14"),  new BigDecimal("-3.14"), true},
+
+                                new Object[] { OnOffType.OFF, DecimalType.valueOf("255"), BigDecimal.valueOf(255), false},
+                                new Object[] { OnOffType.ON, DecimalType.valueOf("255"), BigDecimal.valueOf(255), true},
+                                new Object[] { OnOffType.OFF, DecimalType.valueOf("255"), new BigDecimal("255.0"), false},
+                                new Object[] { OnOffType.ON, DecimalType.valueOf("255"), new BigDecimal("255.0"), true},
+                                new Object[] { OnOffType.ON, DecimalType.valueOf("0"), BigDecimal.valueOf(255), false},
+                                new Object[] { OnOffType.OFF, DecimalType.valueOf("0"), BigDecimal.valueOf(255), true},
+
+                                // We decode into UNDEF number when we have float NaN or float inf. The channel treats
+                                // it same way as any other number.
+                                new Object[] { OnOffType.ON, UnDefType.UNDEF, BigDecimal.valueOf(255), false},
+                                new Object[] { OnOffType.OFF, UnDefType.UNDEF, BigDecimal.valueOf(255), true}
+
+                        //@formatter:on
+        ).collect(Collectors.toList()));
+    }
+
+    // TODO from bits to hexstring
+    // TODO from registers to hexstring
+}
